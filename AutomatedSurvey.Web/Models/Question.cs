7d90b15d32995fd60e0516869fdee59cecdd0d53
GeorgiNik <georgi.nik95@gmail.com>
2017-03-30 17:29:12 +0300

    Transcriptions added

---
+++ AutomatedSurvey.Web/Models/Question.cs
@@ -0,0 +1,25 @@
+﻿namespace AutomatedSurvey.Web.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Question
+    {
+        public virtual IList<Answer> Answers { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public int Id { get; set; }
+
+        public virtual Survey Survey { get; set; }
+
+        public int SurveyId { get; set; }
+
+        [Timestamp]
+        public byte[] Timestamp { get; set; }
+
+        [Required]
+        public QuestionType Type { get; set; }
+    }
+}
